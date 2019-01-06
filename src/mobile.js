57705a1e5a04c122b67b54052fbb6d4b67c7349d
Ner Karso <nerpkarso@gmail.com>
2019-01-07 02:16:21 +0530

    Add mobile version

---
+++ src/mobile.js
@@ -0,0 +1,71 @@
+import Vue from "vue/dist/vue";
+import Table from "../data/table.json";
+
+// Feather Icons
+import feather from "vue-icon";
+Vue.use(feather, "v-icon");
+
+// Component Day View
+const DayView = Vue.component("dayview", {
+  template: "#dayview-template",
+  data() {
+    return {
+      data: Table.data
+    }
+  },
+  methods: {
+    /**
+     * Get Color
+     * 
+     * @param {*} code subject code.
+     * @returns {string} color class.
+     */
+    getColor(code) {
+      switch (code) {
+        case "BSBC201":
+          return "is-yellow";
+          break;
+        case "BSBC202":
+          return "is-turquoise";
+          break;
+        case "BSBC203":
+          return "is-purple";
+          break;
+        case "BSBC204":
+          return "is-cyan";
+          break;
+        case "BSBC205":
+          return "is-orange";
+          break;
+        case "BSBC206":
+          return "is-dark";
+          break;
+        case "EVSC101":
+          return "is-green";
+          break;
+        default:
+          return "is-light";
+      }
+    }
+  }
+});
+
+// App
+new Vue({
+  el: "#app",
+  mounted() {
+    // Update document title
+    try {
+      document.title = document.title.replace("PCTE", Table.class);
+    } catch (erro) {}
+  },
+  data() {
+    return {
+      table: Table,
+      weekDays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    }
+  },
+  components: {
+    DayView
+  }
+});
